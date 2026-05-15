5241abfcc84ec42e0405a2eb26b861b554592687
conestiloprati-design <conestiloprati@gmail.com>
2026-05-15 07:42:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function comprar(producto, precio){
+function comprar(producto,precio){
 
-  const telefono = '5358231944';
+    const telefono='5355555555';
 
-  const mensaje =
-  `Hola, quiero comprar ${producto} con precio ${precio}`;
+    const mensaje=
+    `Hola, quiero comprar ${producto} con precio ${precio}`;
 
-  const url =
-  `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
+    const url=
+    `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
 
-  window.open(url, '_blank');
-
+    window.open(url,'_blank');
 }
