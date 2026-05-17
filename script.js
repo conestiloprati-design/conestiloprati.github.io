d8563c994a8b24dbd95bbc26fe32665b10cfd021
conestiloprati-design <conestiloprati@gmail.com>
2026-05-17 17:30:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function comprar(producto, precio) {
+window.comprar = function(boton) {
+
+    const card = boton.parentElement;
+
+    const nombre = card.querySelector("h3").innerText;
+    const precio = card.querySelector(".precio").innerText;
+    const imagen = card.querySelector("img").src;
 
     const numero = '5358231944';
 
-    const mensaje = `Hola, estoy interesado en comprar: ${producto}
-    $${precio}`;
+    const mensaje = `Hola, quiero comprar:
+
+🛍 ${nombre}
+💲 $${precio}
+📸 ${imagen}`;
 
     const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
 
     window.open(url, '_blank');
 }
-
 
 
 
