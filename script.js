a5dff7618ca68d21a3ee3cd7883c7246b058a1a4
conestiloprati-design <conestiloprati@gmail.com>
2026-05-17 00:19:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function comprar(producto) {
 
     const numero = '5358231944';
 
-    const mensaje = `Hola, estoy interesado en comprar: ${producto}`;
+    const mensaje = `Hola, estoy interesado en comprar: ${producto}
+    $${presio}`;
 
     const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
 
